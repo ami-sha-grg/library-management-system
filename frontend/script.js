d776aa7ff6ba8a2fc9b58bbb4ef1cc85ed745ca3
ami-sha-grg <amisha.785421@prnc.tu.edu.np>
2024-08-10 23:24:19 +0545

    Add files via upload

---
+++ frontend/script.js
@@ -0,0 +1,11 @@
+function adminLogin(){
+    window.location="admin-login.html";
+}
+
+function studentLogin(){
+    window.location="student-registration.html";
+}
+
+function studentRegister(){
+    alert("Student Registerd!")
+}
